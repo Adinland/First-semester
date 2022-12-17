56fa1b325a5fd684d1ac5ba86f748fd9dade40d0
Adinland <116732726+Adinland@users.noreply.github.com>
2022-12-17 20:42:28 +0700

    2.2

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    printf("Enter A, then B\nA <- ");
+    scanf_s("%i", &a);
+    printf("B <- ");
+    scanf_s("%i", &b);
+    if (a > b)
+        printf("A more then B");
+    else if (a == b)
+        printf("A equals B");
+    else
+        printf("B more then A");
+    return 0;
+}
+
+/* https://github.com/Adinland/HomeWork.git */
