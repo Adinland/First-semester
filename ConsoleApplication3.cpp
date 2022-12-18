857c5bad512dc84228c126c7aa6ec00fa9378255
Adinland <116732726+Adinland@users.noreply.github.com>
2022-12-18 14:24:11 +0700

    2.3

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    long long int log, pswd;
+    printf("Enter your login, then password\nlogin <- ");
+    scanf_s("%lli", &log);
+    printf("password <- ");
+    scanf_s("%lli", &pswd);
+    if ((log == 951 && pswd == 753) || (log == 456 && pswd == 654))
+        printf("Permission successfully, welcome _username_");
+    else
+        printf("Permisiion denied, please try again");
+    return 0;
+}
