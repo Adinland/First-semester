12a0c562cce7464ba26f2ece9d77f56138bbd8e3
Adinland <116732726+Adinland@users.noreply.github.com>
2022-12-21 15:14:44 +0700

    2.4

---
+++ ConsoleApplication4.cpp
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int day_of_week;
+    printf("Enter the number of the day of the week\n");
+    scanf_s("%i", &day_of_week);
+    switch (day_of_week)
+    {
+        case 1:
+            printf("Monday ");
+            break;
+        case 2:
+            printf("Tuesday ");
+            break;
+        case 3:
+            printf("Wednesday ");
+            break;
+        case 4:
+            printf("Thursday ");
+            break;
+        case 5:
+            printf("Friday ");
+            break;
+        case 6:
+            printf("Saturday ");
+            break;
+        case 7:
+            printf("Sunday ");
+            break;
+    default:
+        printf("Error, please enter a number from 1 to 7 ");
+        break;
+    }
+    return 0;
+}
