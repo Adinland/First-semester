bb8c12966dec72a064034ed4ae33157cf31934e0
Adinland <116732726+Adinland@users.noreply.github.com>
2023-01-02 23:06:54 +0700

    3.7

---
+++ ConsoleApplication8.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int summ = 0;
+    for (int i = 1; i <= 100; i++)
+        summ += i;
+    cout << "Summ = " << summ << endl;
+    return 0;
+}
