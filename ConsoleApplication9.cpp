507b08dac1e64e5ad96aef95bf2c7b8c0b2f138a
Adinland <116732726+Adinland@users.noreply.github.com>
2023-01-10 18:58:37 +0700

    3.8

---
+++ ConsoleApplication9.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int sum = 0, min = 0, max = 0, len = 0;
+    cout << "Enter the number of elemetns - ";
+    for (;len <= 0;)
+    {
+        cin >> len;
+        if (len <= 0)
+            cout << "Incorrect number" << endl << "Enter the number of elemetns - ";
+    }
+    for (int i = 0, n; i < len; i++)
+    {
+        cout << "-> ";
+        cin >> n;
+        if (i == 0)
+        {
+            min = n;
+            max = n;
+        }
+        sum += n;
+        if (n > max)
+            max = n;
+        else if (n < min)
+            min = n;
+    }
+
+    cout << "Min = " << min << endl << "Max = " << max << endl << "Summ = " << sum << endl << "Average = " << sum / len << endl;
+
+    return 0;
+}
