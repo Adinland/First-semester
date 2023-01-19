649efdb63fc52326ea7cadc26b0a09261aa0a5c1
Adinland <116732726+Adinland@users.noreply.github.com>
2023-01-19 13:42:28 +0700

    5.5 (working functions with an array is here)

---
+++ ConsoleApplication16.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+//функции ввод\вывод массива
+void input(int** arr, int rw, int cl);
+void output(int** arr, int rw, int cl);
+
+int main()
+{
+    int** arr, rw = 0;
+    srand(time(NULL));
+    cout << "Enter the number of columns and rows - ";
+    cin >> rw;
+
+    //создание
+    arr = new int* [rw];
+    for (int i = 0; i < rw; i++)
+    {
+        arr[i] = new int[rw];
+    }
+    input(arr, rw, rw);
+    output(arr, rw, rw);
+
+    //инвертирование по диагонали
+    cout << "Calculating...\n";
+    for (int i = 0; i < rw - 1; i++)
+    {
+        for (int j = i; j < rw - 1; j++)
+        {
+            int buf = arr[i][j + 1];
+            arr[i][j + 1] = arr[j + 1][i];
+            arr[j + 1][i] = buf;
+        }
+    }
+    cout << "Done!\n";
+    output(arr, rw, rw);
+
+    //удаление
+    for (int i = 0; i < rw; i++)
+    {
+        delete[] arr[i];
+    }
+    delete[] arr;
+
+    return 0;
+}
+
+void output(int **arr, int rw, int cl)
+{
+    cout << "\n";
+    for (int i = 0; i < rw; i++)
+    {
+        cout << "| ";
+        for (int j = 0; j < cl; j++)
+        {
+            if (arr[i][j] < 0 || arr[i][j] >= 10)
+                cout << arr[i][j] << " | ";  //ровные грани
+            else
+                cout << arr[i][j] << "  | ";
+        }
+        cout << "\n";
+    }
+    cout << "\n";
+}
+
+void input(int** arr, int rw, int cl)
+{
+    for (int i = 0; i < rw; i++)
+    {
+        for (int j = 0; j < cl; j++)
+        {
+            cout << "-> ";
+            cin >> arr[i][j];
+        }
+    }
+}
