848c1536914d40767eaa61298f3d78cadbe52403
Adinland <116732726+Adinland@users.noreply.github.com>
2023-01-20 16:17:20 +0700

    6.4
    strings, again

---
+++ ConsoleApplication19.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    char buffch;
+    cout << "\tExercise 1:\n";
+    getline(cin, str);
+    str = str.substr(1);
+    cout << str << endl;
+    cout << "\tExercise 2:\n";
+    for (int i = 0, j = str.length() - 1; i < (str.length() / 2); i++, j--)
+    {
+        buffch = str[i];
+        str[i] = str[j];
+        str[j] = buffch;
+    }
+    cout << str << endl;
+
+    return 0;
+}
